bebe62a7d9a89d6c9bd9a45b501ed2570785bca8
abbass19-dev <ahh055lb@aou.edu.lb>
2026-04-17 00:51:28 +0300

    feat: user dashboard implementation

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
-
+//hi
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const encodedSecret = new TextEncoder().encode(JWT_SECRET);
 

38eab3664adad6850c93af4721235d6ce4912a1b
AlialhadiAk <alialhadiabokhalil@gmail.com>
2026-04-21 22:10:45 +0300

    feat(recipe): implement add recipe backend route

---
+++ lib/models/Recipe.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const recipeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    servings: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    prepTime: {
+      type: Number,
+      required: false,
+    },
+    cookTime: {
+      type: Number,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "recipes",
+  },
+);
+
+const Recipe =
+  mongoose.models.Recipe || mongoose.model("Recipe", recipeSchema, "recipes");
+
+export { Recipe };

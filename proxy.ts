d124dfda220c92139976f87971b469c5f88f037a
abbass19-dev <ahh055lb@aou.edu.lb>
2026-05-09 21:24:58 +0300

    fix: exclude specific auth and asset routes from proxy matcher configuration

--- proxy.ts
+++ proxy.ts
@@ -66,5 +66,7 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api/auth/signin|api/auth/signup|api/auth/logout|api/auth/forget-password|api/auth/reset-password|_next/static|_next/image|favicon.ico|manifest.webmanifest|sw.js|icons|images|assets).*)",
+  ],
 };
